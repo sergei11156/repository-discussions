e9b7910f630dacc2f06a6fb1a5db76a480c8db05
sergei <ableevse@gmail.com>
2024-11-28 22:32:33 +0400

    Delete examples

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
-import { runBasicExample } from "../src/commands/basic-example.js";
-import { runChalkExample } from "../src/commands/chalk-example.js";
-import { runInquirerConfirmExample } from "../src/commands/inquirer-confirm-example.js";
-import { runInquirerListExample } from "../src/commands/inquirer-list-example.js";
-import { runOraExample } from "../src/commands/ora-example.js";
 import chalk from "chalk";
 import figlet from "figlet";
 import {runAnalyzeRepository} from "../src/commands/analyze-repository.js";
@@ -31,26 +26,21 @@ const {
 
 spinner.succeed(chalk.green("Hello, " + login + "!"));
 
-const examples = {
+const options = {
   "Analyze Repository": runAnalyzeRepository.bind(this, octokit),
-  "Basic Example": runBasicExample,
-  "Chalk Example": runChalkExample,
-  "Inquirer Confirm Example": runInquirerConfirmExample,
-  "Inquirer List Example": runInquirerListExample,
-  "Ora Example": runOraExample,
 };
 
 inquirer
   .prompt([
     {
       type: "list",
-      name: "selectedExample",
-      message: "Choose an example to run:",
-      choices: Object.keys(examples),
+      name: "selectedCommand",
+      message: "Choose a command to run:",
+      choices: Object.keys(options),
     },
   ])
   .then((answers) => {
-    const exampleFunction = examples[answers.selectedExample];
+    const exampleFunction = options[answers.selectedCommand];
     if (exampleFunction) {
       exampleFunction();
     } else {
