725847199473bee6a38098da85b3cdd1f703f31a
sergei <ableevse@gmail.com>
2024-11-30 16:04:29 +0400

    Add title to issue, getter and setters

--- src/models/issue.js
+++ src/models/issue.js
@@ -1,16 +1,15 @@
-
-
-
 export class Issue {
     get number() {
         return this._number;
     }
+
     _number
     _created_at
     _updated_at
     _closed_at
     _merged_at
     _html_url
+    _title
     _commentsCount
 
     constructor(data) {
@@ -20,7 +19,8 @@ export class Issue {
         this._closed_at = data.closed_at;
         this._merged_at = data.merged_at;
         this._html_url = data.html_url;
-        if (data.commentsCount) {
+        this._title = data.title;
+        if (data.hasOwnProperty("commentsCount")) {
             this._commentsCount = data.commentsCount;
         }
     }
@@ -29,6 +29,7 @@ export class Issue {
     getData() {
         return {
             number: this._number,
+            title: this._title,
             created_at: this._created_at,
             updated_at: this._updated_at,
             closed_at: this._closed_at,
@@ -40,5 +41,17 @@ export class Issue {
 
     setCommentsCount(count) {
         this._commentsCount = count;
+    }
+
+    get commentsCount() {
+        return this._commentsCount;
+    }
+
+    get created_at() {
+        return this._created_at;
+    }
+
+    get title() {
+        return this._title;
     }
 }
