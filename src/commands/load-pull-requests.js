bd6a106c086b1b387fb4b81b22500023fa4ee3f9
sergei <ableevse@gmail.com>
2024-11-30 16:05:37 +0400

    Add status updates

--- src/commands/load-pull-requests.js
+++ src/commands/load-pull-requests.js
@@ -6,7 +6,10 @@ import {chooseRepository} from "../shared/chooseRepository.js";
 
 export const runLoadPullRequests = async () => {
     const repository = await chooseRepository();
-    const spinner = ora('Loading repository pull requests').start()
-    await loadPullRequests(repository);
+    const loadRepositoryPRs = 'Loading repository pull requests.';
+    const spinner = ora(loadRepositoryPRs).start()
+    await loadPullRequests(repository, (page) => {
+        spinner.text = `${loadRepositoryPRs} Page: ${page}`
+    });
     spinner.succeed();
 };
